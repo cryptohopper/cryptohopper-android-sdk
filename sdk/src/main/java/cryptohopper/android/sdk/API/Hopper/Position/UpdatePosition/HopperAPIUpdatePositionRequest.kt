1eab677867b8893e3c45d75e2a6e243dd2eb8af6
Kaan Baris BAYRAK <kaan.bayrak@ozu.edu.tr>
2021-10-26 21:47:15 +0200

    Integer type converted to Long

--- sdk/src/main/java/cryptohopper/android/sdk/API/Hopper/Position/UpdatePosition/HopperAPIUpdatePositionRequest.kt
+++ sdk/src/main/java/cryptohopper/android/sdk/API/Hopper/Position/UpdatePosition/HopperAPIUpdatePositionRequest.kt
@@ -10,7 +10,7 @@ import cryptohopper.android.sdk.SharedModels.ConfigModels.HopperAPIHttpMethod
 
 class HopperAPIUpdatePositionRequest: HopperAPIRequest<HopperCommonMessageResponse> {
     
-    constructor(hopperId : String , positionId:Int,takeProfit : Double,stopLoss:Int,stopLossPercentage : Double,trailingStopLoss : Int, trailingStopLossPercentage : Double,trailingStopLossArm : Double,autoClose : Int,autoCloseTime : Int) {
+    constructor(hopperId : String , positionId:Int,takeProfit : Double,stopLoss:Int,stopLossPercentage : Double,trailingStopLoss : Int, trailingStopLossPercentage : Double,trailingStopLossArm : Double,autoClose : Int,autoCloseTime : Long) {
         this.httpMethod = HopperAPIHttpMethod.POST
         this.needsAuthentication = true
         this.changeUrlPath("/hopper/$hopperId/position/setpositionsetting")
--- sdk/src/main/java/cryptohopper/android/sdk/API/Hopper/Position/UpdateShortPosition/HopperAPIUpdateShortPositionRequest.kt
+++ sdk/src/main/java/cryptohopper/android/sdk/API/Hopper/Position/UpdateShortPosition/HopperAPIUpdateShortPositionRequest.kt
@@ -10,7 +10,7 @@ import cryptohopper.android.sdk.SharedModels.ConfigModels.HopperAPIHttpMethod
 
 class HopperAPIUpdateShortPositionRequest: HopperAPIRequest<HopperCommonMessageResponse> {
     
-    constructor(hopperId : String , shortId:Int , takeProfit : Double,stopLoss : Int,stopLossPercentage : Double,trailingStopLoss : Int, trailingStopLossPercentage : Double,trailingStopLossArm : Double,autoCloseTime : Int,autoRemoveTime : Int) {
+    constructor(hopperId : String , shortId:Int , takeProfit : Double,stopLoss : Int,stopLossPercentage : Double,trailingStopLoss : Int, trailingStopLossPercentage : Double,trailingStopLossArm : Double,autoCloseTime : Long,autoRemoveTime : Long) {
         this.httpMethod = HopperAPIHttpMethod.POST
         this.needsAuthentication = true
         this.changeUrlPath("/hopper/$hopperId/position/setshortsetting")
