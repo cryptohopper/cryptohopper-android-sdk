0c3dde237d299c1ccc6de7ecd236b9d0d50fba48
Malik Dawar <malikdawar@hotmail.com>
2021-11-23 10:32:34 +0300

    CH-841 added the utils for kotlin coroutine

---
+++ sdk/src/test/java/cryptohopper/android/sdk/utils/TestCoroutineRule.kt
@@ -0,0 +1,32 @@
+package cryptohopper.android.sdk.utils
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+@ExperimentalCoroutinesApi
+class TestCoroutineRule : TestRule {
+    val testCoroutineDispatcher = TestCoroutineDispatcher()
+
+    private val testCoroutineScope = TestCoroutineScope(testCoroutineDispatcher)
+
+    override fun apply(base: Statement, description: Description?) = object : Statement() {
+        @Throws(Throwable::class)
+        override fun evaluate() {
+            Dispatchers.setMain(testCoroutineDispatcher)
+            base.evaluate()
+            Dispatchers.resetMain()
+            testCoroutineScope.cleanupTestCoroutines()
+        }
+    }
+
+    fun runBlocking(block: suspend TestCoroutineScope.() -> Unit) =
+        testCoroutineScope.runBlockingTest { block() }
+}
