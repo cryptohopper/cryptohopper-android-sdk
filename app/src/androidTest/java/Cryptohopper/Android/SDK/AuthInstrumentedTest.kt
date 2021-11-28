a2cf152144be6bf07e514e20ba7c5ff1374ac9bd
Malik Dawar <malikdawar@hotmail.com>
2021-11-28 23:28:28 +0100

    CH-603 unit tests for the Cryptohopper auth layer-- in progress

---
+++ app/src/androidTest/java/Cryptohopper/Android/SDK/AuthInstrumentedTest.kt
@@ -0,0 +1,60 @@
+package Cryptohopper.Android.SDK
+
+import Cryptohopper.Android.SDK.helper.Const
+import Cryptohopper.Android.SDK.helper.Const.API_KEY
+import Cryptohopper.Android.SDK.helper.Const.API_PASSWORD
+import Cryptohopper.Android.SDK.helper.Const.API_USER
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.mervick.aes_everywhere.Aes256
+import cryptohopper.android.sdk.CryptohopperAuth
+import cryptohopper.android.sdk.SharedModels.ConfigModels.HopperAPIEnvironment
+import Cryptohopper.Android.SDK.helper.StringGenerator
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.Before
+
+@RunWith(AndroidJUnit4::class)
+class AuthInstrumentedTest {
+
+    @Before
+    fun setup() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        CryptoHopperConfig.configure(
+            appContext, API_KEY,
+            HopperAPIEnvironment.Production
+        )
+    }
+
+    @Test
+    fun when_the_given_login_Endpoint_is_called_with_correct_details_then_it_must_be_successful() {
+        val userAgent = Aes256.encrypt(API_USER, Const.API_AGENT)
+        CryptohopperAuth.login(
+            username = API_USER,
+            password = API_PASSWORD,
+            verificationCode = null,
+            userAgent = userAgent
+        ) { result, error ->
+            Assert.assertNull(error)
+            Assert.assertNotNull(result)
+            Assert.assertEquals("Successfully Logged In", result)
+        }
+    }
+
+    @Test
+    fun when_the_given_login_Endpoint_is_called_with_incorrect_details_then_it_must_not_be_successful() {
+        val userAgent = Aes256.encrypt(StringGenerator.getRandomString(), StringGenerator.getRandomString())
+        CryptohopperAuth.login(
+            username = StringGenerator.getRandomString(),
+            password = StringGenerator.getRandomString(),
+            verificationCode = null,
+            userAgent = userAgent
+        ) { result, error ->
+            Assert.assertNull(result)
+            Assert.assertNotNull(error)
+            Assert.assertEquals(HopperError.UNAUTHORIZED, error?.error)
+            Assert.assertEquals(401, error?.errCode)
+        }
+    }
+}
