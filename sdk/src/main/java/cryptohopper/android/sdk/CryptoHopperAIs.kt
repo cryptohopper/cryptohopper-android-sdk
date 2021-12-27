db27354f28bac2fb902b451aacfd53c46d36055e
Malik Dawar <malikdawar@hotmail.com>
2021-12-27 15:51:52 +0100

    CH-977 added the new endpoint for the hopper AIs

---
+++ sdk/src/main/java/cryptohopper/android/sdk/CryptoHopperAIs.kt
@@ -0,0 +1,24 @@
+import cryptohopper.android.sdk.API.AI.General.GetAIs.HopperAPIGetAIsDataItem
+import cryptohopper.android.sdk.API.AI.General.GetAIs.HopperAPIGetAIsResponse
+import cryptohopper.android.sdk.SharedModels.ConfigModels.HopperAPIError
+
+/**
+ * CryptoHopperAIs.kt
+ * @author Malik Dawar
+ */
+
+object CryptoHopperAIs {
+
+    /**
+     * fun getHopperAIs, to get the list of all the hopper AIs
+     * @return lambda as, callback: (HopperAPIGetAIsResponse?, HopperAPIError?) -> Unit
+     * @author Malik Dawar
+     */
+    fun getHopperAIs(callback: (List<HopperAPIGetAIsDataItem>?, HopperAPIError?) -> Unit) {
+        HopperAPIGetAIsRequest().request<HopperAPIGetAIsResponse>({ hopperAIs ->
+            callback(hopperAIs.data, null)
+        }, { error ->
+            callback(null, error)
+        })
+    }
+}
