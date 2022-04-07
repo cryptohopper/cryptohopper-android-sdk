29879ebab238354a64b4fd9131a9ec4aa0ba6eb9
canerten <canerten94@gmail.com>
2022-04-07 23:43:26 +0300

    Position Settings Update Request
    HopperAPIUpdatePositionRequest
    HopperAPIUpdateShortPositionRequest

--- sdk/src/main/java/cryptohopper/android/sdk/API/Hopper/Position/UpdatePosition/HopperAPIUpdatePositionRequest.kt
+++ sdk/src/main/java/cryptohopper/android/sdk/API/Hopper/Position/UpdatePosition/HopperAPIUpdatePositionRequest.kt
@@ -10,40 +10,14 @@ import cryptohopper.android.sdk.SharedModels.ConfigModels.HopperAPIHttpMethod
 
 class HopperAPIUpdatePositionRequest: HopperAPIRequest<HopperCommonMessageResponse> {
     
-    constructor(hopperId : String , positionId:Int,takeProfit : Double,stopLoss:Int,stopLossPercentage : Double,trailingStopLoss : Int, trailingStopLossPercentage : Double,trailingStopLossArm : Double,autoClose : Int,autoCloseTime : Long) {
+    constructor(hopperId : String , positionId:Int, values : MutableMap<String,Any>) {
         this.httpMethod = HopperAPIHttpMethod.POST
         this.needsAuthentication = true
         this.changeUrlPath("/hopper/$hopperId/position/setpositionsetting")
 
         addBodyItem( "position_ids", listOf<Int>(positionId))
 
-        var settings = mutableMapOf<String,Any>()
-
-        settings["take_profit"] = takeProfit
-
-        settings["stop_loss_enabled"] = stopLoss
-        if(stopLoss == 1){
-            settings["stop_loss_percentage"] = stopLossPercentage
-        }else{
-            settings["stop_loss_percentage"] = 0.0
-        }
-
-        settings["trailing_enabled"] = trailingStopLoss
-        if(trailingStopLoss == 1){
-            settings["trailing_percentage"] = trailingStopLossPercentage
-            settings["trailing_arm_percentage"] = trailingStopLossArm
-        }else{
-            settings["trailing_percentage"] = 0.0
-            settings["trailing_arm_percentage"] = 0.0
-        }
-
-        if(autoClose == 1){
-            settings["auto_close_time"] = autoCloseTime
-        }else{
-            settings["auto_close_time"] = 0
-        }
-
-        addBodyItem( "setting", settings)
+        addBodyItem( "setting", values)
     }
     
 }
--- sdk/src/main/java/cryptohopper/android/sdk/API/Hopper/Position/UpdateShortPosition/HopperAPIUpdateShortPositionRequest.kt
+++ sdk/src/main/java/cryptohopper/android/sdk/API/Hopper/Position/UpdateShortPosition/HopperAPIUpdateShortPositionRequest.kt
@@ -10,37 +10,14 @@ import cryptohopper.android.sdk.SharedModels.ConfigModels.HopperAPIHttpMethod
 
 class HopperAPIUpdateShortPositionRequest: HopperAPIRequest<HopperCommonMessageResponse> {
     
-    constructor(hopperId : String , shortId:Int , takeProfit : Double,stopLoss : Int,stopLossPercentage : Double,trailingStopLoss : Int, trailingStopLossPercentage : Double,trailingStopLossArm : Double,autoCloseTime : Long,autoRemoveTime : Long) {
+    constructor(hopperId : String , shortId:Int, values: MutableMap<String,Any>) {
         this.httpMethod = HopperAPIHttpMethod.POST
         this.needsAuthentication = true
         this.changeUrlPath("/hopper/$hopperId/position/setshortsetting")
 
         addBodyItem( "position_ids", listOf<Int>(shortId))
 
-        var settings = mutableMapOf<String,Any>()
-
-        settings["take_profit"] = takeProfit
-
-        settings["stop_loss_enabled"] = stopLoss
-        if(stopLoss == 1){
-            settings["stop_loss_percentage"] = stopLossPercentage
-        }else{
-            settings["stop_loss_percentage"] = 0.0
-        }
-
-        settings["trailing_enabled"] = trailingStopLoss
-        if(trailingStopLoss == 1){
-            settings["trailing_percentage"] = trailingStopLossPercentage
-            settings["trailing_arm_percentage"] = trailingStopLossArm
-        }else{
-            settings["trailing_percentage"] = 0.0
-            settings["trailing_arm_percentage"] = 0.0
-        }
-
-        settings["auto_close_time"] = autoCloseTime
-        settings["auto_remove_time"] = autoRemoveTime
-
-        addBodyItem("setting", settings)
+        addBodyItem("setting", values)
     }
     
 }
