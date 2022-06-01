fa300587e07770ce059e6de4e7bd6d505f62e6bc
malikdawar <malikdawar@hotmail.com>
2022-06-01 20:51:26 +0200

    completed the exchange APIs

--- sdk/src/main/java/cryptohopper/android/sdk/API/apiv2/exchange/allv2exchange/HopperAPIV2GetAllExchangesRequest.kt
+++ sdk/src/main/java/cryptohopper/android/sdk/API/apiv2/exchange/allv2exchange/HopperAPIV2GetAllExchangesRequest.kt
@@ -1,14 +1,14 @@
-package cryptohopper.android.sdk.API.apiv2.currencies
+package cryptohopper.android.sdk.API.apiv2.exchange.allv2exchange
 
 
 import HopperAPIRequest
 import cryptohopper.android.sdk.SharedModels.ConfigModels.HopperAPIHttpMethod
 
-class HopperAPIV2GetAllCurrenciesRequest : HopperAPIRequest<List<V2Currency>> {
+class HopperAPIV2GetAllExchangesRequest : HopperAPIRequest<List<V2Exchange>> {
 
     constructor(data: String? = null) {
         this.httpMethod = HopperAPIHttpMethod.GET
         this.needsAuthentication = true
-        this.changeUrlPath(path = "/rest/api/v2/currencies/list", isV2Endpoint = true)
+        this.changeUrlPath(path = "/rest/api/v2/exchange/list", isV2Endpoint = true)
     }
 }
---
+++ sdk/src/main/java/cryptohopper/android/sdk/API/apiv2/exchange/allv2exchange/V2Exchange.kt
@@ -0,0 +1,39 @@
+package cryptohopper.android.sdk.API.apiv2.exchange.allv2exchange
+
+import com.google.gson.annotations.SerializedName
+
+data class V2Exchange(
+    @SerializedName("id") val id: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("key") val key: String?,
+    @SerializedName("description") val description: String?,
+    @SerializedName("signupLink") val signupLink: String?,
+    @SerializedName("tutorialLink") val tutorialLink: String?,
+    @SerializedName("defaultQuoteCurrency") val defaultQuoteCurrency: String?,
+    @SerializedName("totalHoppers") val totalHoppers: Int?,
+    @SerializedName("totalMarkets") val totalMarkets: Int?,
+    @SerializedName("totalTrades") val totalTrades: Int?,
+    @SerializedName("uptimeLastHour") val uptimeLastHour: Double?,
+    @SerializedName("uptimeLastWeek") val uptimeLastWeek: Double?,
+    @SerializedName("uptimeLastMonth") val uptimeLastMonth: Double?,
+    @SerializedName("createdAt") val createdAt: String?,
+    @SerializedName("updatedAt") val updatedAt: String?,
+    @SerializedName("ordering") val ordering: Int?,
+    @SerializedName("premiumPartner") val premiumPartner: Boolean?,
+    @SerializedName("exchangeStatus") val exchangeStatus: Boolean?,
+    @SerializedName("availableSoon") val availableSoon: Boolean?,
+    @SerializedName("isBeta") val isBeta: Boolean?,
+    @SerializedName("isCeta") val isCeta: Boolean?,
+    @SerializedName("status") val status: Boolean?,
+    @SerializedName("display") val display: Boolean?,
+    @SerializedName("maintenance") val maintenance: Boolean?,
+    @SerializedName("margin") val margin: Boolean?,
+    @SerializedName("extraLinks") val extraLinks: List<String>?,
+    @SerializedName("quoteCurrencies") val quoteCurrencies: List<String>?,
+    @SerializedName("images") val images: List<Map<String?, String?>>?,
+    @SerializedName("apiConfigFields") val apiConfigFields: List<String>?,
+    @SerializedName("optionalConfigFields") val optionalConfigFields: List<String>?
+)
+
+
+
