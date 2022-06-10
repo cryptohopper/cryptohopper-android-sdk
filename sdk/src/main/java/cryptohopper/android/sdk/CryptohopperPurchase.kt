57d510349f863bf068c50fbb5538d212e4631508
canerten <canerten94@gmail.com>
2022-06-10 13:36:38 +0200

    IAP Endpoint Added

--- sdk/src/main/java/cryptohopper/android/sdk/CryptohopperPurchase.kt
+++ sdk/src/main/java/cryptohopper/android/sdk/CryptohopperPurchase.kt
@@ -3,6 +3,7 @@ package cryptohopper.android.sdk
 import HopperError
 import cryptohopper.android.sdk.API.Purchase.General.HopperAPIPurchaseRequest
 import cryptohopper.android.sdk.API.Purchase.General.HopperAPIPurchaseResponse
+import cryptohopper.android.sdk.API.Purchase.IAP.HopperAPIIAPPurchaseRequest
 import cryptohopper.android.sdk.SharedModels.ConfigModels.HopperAPIError
 
 class CryptohopperPurchase {
@@ -13,6 +14,14 @@ class CryptohopperPurchase {
         ///
         fun purchase(planId: String?,marketplaceId: String?,hopperId: String?,receipt: String, callback : (HopperAPIPurchaseResponse?, HopperAPIError?) -> Unit) {
             HopperAPIPurchaseRequest(planId,marketplaceId,hopperId,receipt).request<HopperAPIPurchaseResponse>({ data ->
+                callback(data,null)
+            } , { error ->
+                callback(null,error)
+            })
+        }
+
+        fun purchaseIAP(planId: String?,marketplaceId: String?,hopperId: String?,transactionId: String, callback : (HopperAPIPurchaseResponse?, HopperAPIError?) -> Unit) {
+            HopperAPIIAPPurchaseRequest(planId,marketplaceId,hopperId,transactionId).request<HopperAPIPurchaseResponse>({ data ->
                 callback(data,null)
             } , { error ->
                 callback(null,error)
